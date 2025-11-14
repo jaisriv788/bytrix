c480702e5c24b40bf90181c27edbb39ca2d20132
jaisriv788 <srivastavajai845@gmail.com>
2025-11-14 18:13:59 +0530

    feat: completed the integration of the lease contract with the testing

--- src/components/lease/Hero.jsx
+++ src/components/lease/Hero.jsx
@@ -1,34 +1,42 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import videoBg from "../../assets/v3.mp4";
 import { useSelector } from 'react-redux';
 import { ethers, formatUnits } from 'ethers';
-// import contractAbi from "../../contractAbi.json";
+import contractAbi from "../../leaseAbi.json";
 import erc20Abi from "../../erc20Abi.json";
 import { useNotification } from '../../hooks/useNotification';
+import { useParams } from 'react-router';
+import useEthers from '../../hooks/useEthers';
 // import { CircleQuestionMark } from 'lucide-react';
 
-function Lease({ showModal }) {
+function Lease({ showModal, setReloadData }) {
     const defaultPlans = [
-        { name: "1 Day", clicked: true, percentage: 0.3, planId: 1, fees: 1, min: 10, max: 250 },
-        { name: "7 Days", clicked: false, percentage: 3, planId: 2, fees: 1, min: 50, max: 1000 },
-        { name: "15 Days", clicked: false, percentage: 7.5, planId: 3, fees: 1, min: 50, max: 5000 },
-        { name: "30 Days", clicked: false, percentage: 20, planId: 4, fees: 1, min: 100, max: 10000 },
+        { name: "1 Day", clicked: true, percentage: 2, planId: 1, fees: 1, min: 10, max: 250 },
+        { name: "7 Days", clicked: false, percentage: 10, planId: 2, fees: 1, min: 50, max: 1000 },
+        { name: "15 Days", clicked: false, percentage: 20, planId: 3, fees: 1, min: 50, max: 5000 },
+        { name: "30 Days", clicked: false, percentage: 36, planId: 4, fees: 1, min: 100, max: 10000 },
     ];
+
+    const { ref } = useParams();
 
     const [dataBox, setDataBox] = useState(defaultPlans);
     const [balanceLoading, setBalanceLoading] = useState(false);
     const [usdtBalance, setUsdtBalance] = useState(0);
-    const [amount, setAmount] = useState("");
+    const [borrowAmount, setBorrowAmount] = useState("");
     const [refetch, setRefetch] = useState(false);
     const [loading, setLoading] = useState(false);
     const [selectedBox, setSelectedBox] = useState(defaultPlans[0]);
 
     const isConnected = useSelector((state) => state.user.isConnected);
     const USDTAddress = useSelector((state) => state.user.USDTAddress);
-    // const contractAddress = useSelector((state) => state.user.contractAddress);
+    const contractAddress = useSelector((state) => state.user.leaseConstractAddress);
     const walletAddress = useSelector((state) => state.user.walletAddress);
+    const companyWalletAddress = useSelector(
+        (state) => state.user.companyWalletAddress
+    );
 
-    const { showError } = useNotification();
+    const { showError, showSuccess } = useNotification();
+    const { signer } = useEthers();
 
     useEffect(() => {
         const getPrice = async () => {
@@ -59,7 +67,7 @@ function Lease({ showModal }) {
 
     const handleInputChange = (e) => {
         const value = e.target.value;
-        setAmount(value);
+        setBorrowAmount(value);
     };
 
     const handleClick = (index) => {
@@ -72,9 +80,45 @@ function Lease({ showModal }) {
     };
 
     async function handleSubmit() {
-        console.log(amount, selectedBox)
-        setRefetch(false)
-        setLoading(false)
+        try {
+            if (borrowAmount < 1) {
+                showError("Amount Is Less Than 1!");
+                return;
+            }
+
+            const refrealAddress = ref ? ref : companyWalletAddress;
+
+            if (!ethers.isAddress(refrealAddress)) {
+                showError("Wrong Referral Link Or Address!");
+                return;
+            }
+            setLoading(true);
+
+            const tokenContract = new ethers.Contract(USDTAddress, erc20Abi, signer);
+            const contract = new ethers.Contract(contractAddress, contractAbi, signer);
+
+            const planid = selectedBox?.planId;
+            const originalAmount = ethers.parseUnits(borrowAmount, 18);
+
+            console.log({ planid, originalAmount })
+
+            const tx = await tokenContract.approve(contractAddress, originalAmount);
+            await tx.wait();
+
+            const tx2 = await contract.startLease(originalAmount, planid, {
+                gasLimit: 500000,
+            });
+            await tx2.wait();
+
+            showSuccess("Transaction Successful!");
+        } catch (error) {
+            console.log(error)
+            showError("Transaction Failed");
+        } finally {
+            setRefetch((prev) => !prev);
+            setReloadData((prev) => !prev);
+            setLoading(false)
+        }
     }
 
     return (
@@ -111,165 +155,110 @@ function Lease({ showModal }) {
                     Lease
                 </span>
 
-                <div className="tabs tabs-border">
-                    <input type="radio" name="my_tabs_2" className="tab text-white hover:text-gray-300 transition ease-in-out duration-300" aria-label="Borrow" defaultChecked />
-                    <div className="tab-content"> {/* Amount input */}
-                        <div>
-                            <div className="flex justify-between mt-3">
-                                <span className="font-semibold">Borrow Amount</span>
-                                <span>
-                                    <span className="text-gray-400">Balance</span>{" "}
-                                    {balanceLoading ? "0.0000" : parseFloat(usdtBalance).toFixed(4)} -
-                                    USDT
-                                </span>
-                            </div>
 
-                            <div className="bg-slate-600 focus-within:bg-gradient-to-tr focus-within:from-[#00BFFF] focus-within:to-[#00FFFF] p-1 mt-2 rounded-xl">
-                                <div className="flex gap-2 px-2 bg-slate-600 rounded-lg items-center">
-                                    <input
-                                        value={amount}
-                                        onChange={handleInputChange}
-                                        disabled={loading}
-                                        className="flex-1 border-none py-2.5 focus:outline-none text-lg focus:text-[#00FFFF] font-semibold bg-transparent"
-                                        type="number"
-                                        placeholder="Enter Borrow Amount"
-                                    />
-                                    <button
-                                        onClick={() => {
-                                            let floored =
-                                                Math.floor(parseFloat(usdtBalance) * 10000) / 10000;
-                                            setAmount(floored.toFixed(4));
-                                        }}
-                                        className="bg-white h-fit text-black px-2 rounded cursor-pointer hover:bg-white/70 transition ease-in-out duration-300"
-                                    >
-                                        Max
-                                    </button>
-                                </div>
-                            </div>
+                <div className=""> {/* Amount input */}
+                    <div>
+                        <div className="flex justify-between mt-3">
+                            <span className="font-semibold">Borrow Amount</span>
+                            <span>
+                                <span className="text-gray-400">Balance</span>{" "}
+                                {balanceLoading ? "0.0000" : parseFloat(usdtBalance).toFixed(4)} -
+                                USDT
+                            </span>
                         </div>
 
-                        {/* Duration options */}
-                        <div>
-                            <div className="flex justify-between mt-3">
-                                <span className="font-semibold">Duration</span>
-                            </div>
-                            <div className="flex flex-wrap justify-center gap-2">
-                                {dataBox.map((item, index) => (
-                                    <div
-                                        key={index}
-                                        onClick={() => handleClick(index)}
-                                        className={`p-1 min-w-25 ${item.clicked
-                                            ? "bg-gradient-to-tr from-[#00BFFF] to-[#00FFFF]"
-                                            : "bg-slate-500"
-                                            } cursor-pointer rounded-full`}
-                                    >
-                                        <div
-                                            className={`px-3 py-2 font-semibold rounded-full text-center ${!item.clicked && "bg-slate-500"
-                                                }`}
-                                        >
-                                            {item.name}
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-
-                        {/* Fees & Buttons */}
-                        <div className="mb-1">
-                            {/* <div className="flex justify-between mt-3">
-                        {dataBox.map(
-                            (item, index) =>
-                                item.clicked && (
-                                    <span key={index} className="font-semibold">
-                                        {item.name}
-                                    </span>
-                                )
-                        )}
-                        <span>
-                            <span className="text-gray-400 font-bold">
-                                ~{usdtValue ? parseFloat(usdtValue).toFixed(4) : "0.0000"}
-                            </span>{" "}
-                            - USDT
-                        </span>
-                    </div> */}
-
-                            {isConnected ? (
+                        <div className="bg-slate-600 focus-within:bg-gradient-to-tr focus-within:from-[#00BFFF] focus-within:to-[#00FFFF] p-1 mt-2 rounded-xl">
+                            <div className="flex gap-2 px-2 bg-slate-600 rounded-lg items-center">
+                                <input
+                                    value={borrowAmount}
+                                    onChange={handleInputChange}
+                                    disabled={loading}
+                                    className="flex-1 border-none py-2.5 focus:outline-none text-lg focus:text-[#00FFFF] font-semibold bg-transparent"
+                                    type="number"
+                                    placeholder="Enter Borrow Amount"
+                                />
                                 <button
-                                    onClick={handleSubmit}
-                                    disabled={!amount}
-                                    className="disabled:opacity-50 disabled:cursor-not-allowed bg-gradient-to-tr from-[#00BFFF] to-[#00FFFF] mt-3 w-full py-2 rounded-full font-bold text-black/80 cursor-pointer hover:scale-103 hover:-translate-y-0.5 transition ease-in-out duration-200"
+                                    onClick={() => {
+                                        let floored =
+                                            Math.floor(parseFloat(usdtBalance) * 10000) / 10000;
+                                        setBorrowAmount(floored.toFixed(4));
+                                    }}
+                                    className="bg-white h-fit text-black px-2 rounded cursor-pointer hover:bg-white/70 transition ease-in-out duration-300"
                                 >
-                                    Borrow
+                                    Max
                                 </button>
-                            ) : (
-                                <button
-                                    onClick={() => showModal(true)}
-                                    className="bg-gradient-to-tr from-[#00BFFF] to-[#00FFFF] mt-3 w-full py-2 rounded-full font-bold text-black/90 cursor-pointer hover:scale-103 hover:-translate-y-0.5 transition ease-in-out duration-200"
-                                >
-                                    Connect Wallet
-                                </button>
-                            )}
-
-                            {loading && (
-                                <div className="text-center mt-3 font-bold text-red-400">
-                                    Please Do Not Close The Tab!
-                                </div>
-                            )}
+                            </div>
                         </div>
                     </div>
 
-                    <input type="radio" name="my_tabs_2" className="tab text-white hover:text-gray-300 transition ease-in-out duration-300" aria-label="Pay" />
-                    <div className="tab-content"> {/* Amount input */}
-                        <div>
-                            <div className="bg-slate-600 focus-within:bg-gradient-to-tr focus-within:from-[#00BFFF] focus-within:to-[#00FFFF] p-1 mt-2 rounded-xl">
-                                <div className="flex gap-2 px-2 bg-slate-600 rounded-lg items-center">
-                                    <input
-                                        value={amount}
-                                        onChange={handleInputChange}
-                                        disabled={loading}
-                                        className="flex-1 border-none py-2.5 focus:outline-none text-lg focus:text-[#00FFFF] font-semibold bg-transparent"
-                                        type="number"
-                                        placeholder="Enter Returning Amount"
-                                    />
-                                    <button
-                                        onClick={() => {
-                                            let floored =
-                                                Math.floor(parseFloat(usdtBalance) * 10000) / 10000;
-                                            setAmount(floored.toFixed(4));
-                                        }}
-                                        className="bg-white h-fit text-black px-2 rounded cursor-pointer hover:bg-white/70 transition ease-in-out duration-300"
+                    {/* Duration options */}
+                    <div>
+                        <div className="flex justify-between mt-3">
+                            <span className="font-semibold">Duration</span>
+                        </div>
+                        <div className="flex flex-wrap justify-center gap-2">
+                            {dataBox.map((item, index) => (
+                                <div
+                                    key={index}
+                                    onClick={() => handleClick(index)}
+                                    className={`p-1 min-w-25 ${item.clicked
+                                        ? "bg-gradient-to-tr from-[#00BFFF] to-[#00FFFF]"
+                                        : "bg-slate-500"
+                                        } cursor-pointer rounded-full`}
+                                >
+                                    <div
+                                        className={`px-3 py-2 font-semibold rounded-full text-center ${!item.clicked && "bg-slate-500"
+                                            }`}
                                     >
-                                        Max
-                                    </button>
+                                        {item.name}
+                                    </div>
                                 </div>
-                            </div>
+                            ))}
                         </div>
-                        <div className="mb-1">
-                            {isConnected ? (
-                                <button
-                                    // onClick={handleSubmit}
-                                    // disabled={!amount}
-                                    className="disabled:opacity-50 disabled:cursor-not-allowed bg-gradient-to-tr from-[#00BFFF] to-[#00FFFF] mt-3 w-full py-2 rounded-full font-bold text-black/80 cursor-pointer hover:scale-103 hover:-translate-y-0.5 transition ease-in-out duration-200"
-                                >
-                                    Pay
-                                </button>
-                            ) : (
-                                <button
-                                    onClick={() => showModal(true)}
-                                    className="bg-gradient-to-tr from-[#00BFFF] to-[#00FFFF] mt-3 w-full py-2 rounded-full font-bold text-black/90 cursor-pointer hover:scale-103 hover:-translate-y-0.5 transition ease-in-out duration-200"
-                                >
-                                    Connect Wallet
-                                </button>
-                            )}
+                    </div>
 
-                            {loading && (
-                                <div className="text-center mt-3 font-bold text-red-400">
-                                    Please Do Not Close The Tab!
-                                </div>
-                            )}
-                        </div>
+                    <div className='flex justify-between mt-2'>
+                        <div>Intrest Percentage</div>
+                        <div>{selectedBox?.percentage}%</div>
+                    </div>
+                    <div className='flex justify-between mt-1'>
+                        <div>Intrest Amount</div>
+                        <div>${borrowAmount * selectedBox.percentage / 100}</div>
+                    </div>
+                    <div className='flex justify-between mt-1'>
+                        <div>Total Amount</div>
+                        <div>${parseFloat(borrowAmount) + (borrowAmount * selectedBox.percentage / 100)}</div>
+                    </div>
+
+                    {/* Fees & Buttons */}
+                    <div className="mb-1">
+                        {isConnected ? (
+                            <button
+                                onClick={handleSubmit}
+                                disabled={!borrowAmount || loading}
+                                className="disabled:opacity-50 disabled:cursor-not-allowed bg-gradient-to-tr from-[#00BFFF] to-[#00FFFF] mt-3 w-full py-2 rounded-full font-bold text-black/80 cursor-pointer hover:scale-103 hover:-translate-y-0.5 transition ease-in-out duration-200"
+                            >
+                                {loading ? <span className="loading loading-spinner loading-md"></span> : "Borrow"}
+                            </button>
+                        ) : (
+                            <button
+                                onClick={() => showModal(true)}
+                                className="bg-gradient-to-tr from-[#00BFFF] to-[#00FFFF] mt-3 w-full py-2 rounded-full font-bold text-black/90 cursor-pointer hover:scale-103 hover:-translate-y-0.5 transition ease-in-out duration-200"
+                            >
+                                Connect Wallet
+                            </button>
+                        )}
+
+                        {loading && (
+                            <div className="text-center mt-3 font-bold text-red-400">
+                                Please Do Not Close The Tab!
+                            </div>
+                        )}
                     </div>
                 </div>
+
+
+
             </div>
         </div>
     )
--- src/screens/Lease.jsx
+++ src/screens/Lease.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react"
 import Hero from "../components/lease/Hero"
 import { Orders } from "../components/lease/Orders"
 
 function Lease({ showModal }) {
+    const [reloadData, setReloadData] = useState(false)
     return (
         <div>
-            <Hero showModal={showModal} />
-            <Orders />
+            <Hero showModal={showModal} setReloadData={setReloadData} />
+            <Orders reloadData={reloadData} />
         </div>
     )
 }
